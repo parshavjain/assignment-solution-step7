7793b3226b3c1c5a643ec2b29a6ad61b20a7e7a2
Abbas <abbas@yahoo.com>
2017-08-24 14:55:07 +0530

    Update comments.

--- src/com/stackroute/datamunger/query/parser/Restriction.java
+++ src/com/stackroute/datamunger/query/parser/Restriction.java
@@ -1,5 +1,17 @@
 package com.stackroute.datamunger.query.parser;
 
+/**
+ * This class is the custom class to hole 'where' condition detais.
+ * 'Where' conditin  contains 3 items called properName, propertyValue and condition
+ * So create private fields and generate getter/setter methods.
+ * private String propertyName;
+ * private String propertyValue;
+ * private String condition;
+ * 
+ * Note:  This is POJO class(A class which has properties and getter/setter methods)
+ * No other logic should present in this class.
+ * 
+ * */
 public class Restriction {
 	
 	
