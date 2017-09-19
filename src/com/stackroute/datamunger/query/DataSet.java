31d90311df02aa32842ac3d7fc473429447cd7d1
abbas886 <abbas886@yahoo.com>
2017-09-19 11:38:00 +0530

    Added properties and generated getter/setter methods

--- src/com/stackroute/datamunger/query/DataSet.java
+++ src/com/stackroute/datamunger/query/DataSet.java
@@ -31,5 +31,39 @@ public class DataSet {
 	private Map<String, List<List<String>>> groupByResult;
 	
 	private Map<String, DoubleSummaryStatistics> groupByAggregateResult;
+	
+	
+
+	public Map<String, DoubleSummaryStatistics> getGroupByAggregateResult() {
+		return groupByAggregateResult;
+	}
+
+	public void setGroupByAggregateResult(Map<String, DoubleSummaryStatistics> groupByAggregateResult) {
+		this.groupByAggregateResult = groupByAggregateResult;
+	}
+
+	public Map<String, List<List<String>>> getGroupByResult() {
+		return groupByResult;
+	}
+
+	public void setGroupByResult(Map<String, List<List<String>>> groupByResult) {
+		this.groupByResult = groupByResult;
+	}
+
+	public List<List<String>> getResult() {
+		return result;
+	}
+
+	public void setResult(List<List<String>> result) {
+		this.result = result;
+	}
+
+	public List<AggregateFunction> getAggregateFunctions() {
+		return aggregateFunctions;
+	}
+
+	public void setAggregateFunctions(List<AggregateFunction> aggregateFunctions) {
+		this.aggregateFunctions = aggregateFunctions;
+	}	
 
 }
--- src/com/stackroute/datamunger/query/parser/Restriction.java
+++ src/com/stackroute/datamunger/query/parser/Restriction.java
@@ -14,5 +14,27 @@ package com.stackroute.datamunger.query.parser;
  * */
 public class Restriction {
 	
+
+	
+	private String propertyName;
+	private String propertyValue;
+	private String condition;
+	public Restriction(String propertyName, String propertyValue, String condition) {
+		this.propertyName = propertyName;
+		this.propertyValue = propertyValue;
+		this.condition = condition;
+	}
+	public String getPropertyName() {
+		return propertyName;
+	}
+	public String getPropertyValue() {
+		return propertyValue;
+	}
+	public String getCondition() {
+		return condition;
+	}
+
+
+	
 	
 }
